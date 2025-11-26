fa153e700cfe87766a4eee867c66fd42eddfd45d
devniki-hi <hiniki.dev@gmail.com>
2025-11-26 14:48:22 +0900

    [add] Deviseをセットアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  devise_for :users,
+              skip: [ :sessions, :registrations ],
+              controllers: {
+                sessions: "users/sessions",
+                registrations: "users/registrations"
+              }
+
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "static_pages#welcome", as: :unauthenticated_root
+  end
+
+  # カスタムパスを設定（asはDeviseが内部で期待する名前ルートを維持するため）
+  devise_scope :user do
+    # セッション用ルーティング
+    get "/login", to: "users/sessions#new", as: "new_user_session"
+    post "/login", to: "users/sessions#create", as: "user_session"
+    delete "/logout", to: "users/sessions#destroy", as: "destroy_user_session"
+
+    # アカウント登録・編集用ルーティング（プロフィールとは別物）
+    get "/signup", to: "users/registrations#new", as: "new_user_registration"
+    post "/signup", to: "users/registrations#create", as: "user_registration"
+    put "/account", to: "users/registrations#update", as: "update_user_registration"
+    delete "/account", to: "users/registrations#destroy", as: "destroy_user_registration"
+  end
   # Redirect to localhost from 127.0.0.1 to use same IP address with Vite server
   get "inertia-example", to: "inertia_example#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# 以下を追加
+User.create!(
+  name: "Test user",
+  email: "test@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+# ======== その他 4 ユーザー（ダミー）========
+4.times do |i|
+  User.create!(
+    name: "#{Faker::Name.name}",
+    email: "user#{i + 2}@example.com",
+    password: "password#{i + 2}",
+    password_confirmation: "password#{i + 2}"
+  )
+end
