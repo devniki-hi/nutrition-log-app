89a883a211845709bf8d1a4dd0be22cdaf1122ab
devniki-hi <hiniki.dev@gmail.com>
2025-12-08 10:40:13 +0900

    [add] foodのバリデーションを追加

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -12,7 +12,8 @@ class FoodsController < InertiaController
     render inertia: "Foods/Index", props: {
       foods: @foods.map do |food|
         serialize_food(food)
-      end
+      end,
+      errors: flash[:errors] ? { errors: flash[:errors] }: {}
     }
   end
 
@@ -29,25 +30,27 @@ class FoodsController < InertiaController
     render inertia: "Foods/New", props: {
       food: @food,
       unit_types: Food.unit_types.keys,
-      sources: Food.sources.keys
+      sources: Food.sources.keys,
+      errors: flash[:errors] ? { errors: flash[:errors] }: {}
     }
   end
 
   # GET /foods/1/edit
   def edit
     render inertia: "Foods/Edit", props:  {
-      food: serialize_food(@food)
+      food: serialize_food(@food),
+      errors: flash[:errors] ? { errors: flash[:errors] }: {}
     }
   end
 
   # POST /foods
   def create
     @food = Food.new(food_params)
-
     if @food.save
       redirect_to @food, notice: "食品が保存されました。"
     else
-      redirect_to new_food_url, inertia: { errors: @food.errors }
+      flash[:errors] = @food.errors
+      redirect_to new_food_url, inertia: { errors: flash[:errors]}
     end
   end
 
@@ -56,7 +59,8 @@ class FoodsController < InertiaController
     if @food.update(food_params)
       redirect_to @food, notice: "食品が更新されました。"
     else
-      redirect_to edit_food_url(@food), inertia: { errors: @food.errors }
+      flash[:errors] = @food.errors
+      redirect_to edit_food_url(@food), inertia: { errors: flash[:errors] }
     end
   end
 
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,8 @@
 class Food < ApplicationRecord
   has_many :meal_logs, dependent: :destroy
   has_many :users, through: :meal_logs
+  before_validation :set_default_nutrition_values
+
   enum :unit_type, {
     g: 0,
     piece: 1,
@@ -11,4 +13,62 @@ class Food < ApplicationRecord
     api: 1,
     jan: 2
   }
+  
+  validates :name,
+    presence: true,
+    length: { maximum: 30 }
+
+  with_options numericality: {
+    greater_than_or_equal_to: 0,
+    format: { with: /\A\d+(\.\d{1,2})?\z/ } 
+  } do
+    validates :kcal
+    validates :protein
+    validates :fat
+    validates :carbs
+    validates :sugar
+    validates :fiber
+  end
+
+ validates :portion_value,
+    numericality: {
+      only_integer: true,
+      message: "整数で入力してください"
+    }
+
+  validates :portion_value,
+    numericality: {
+      greater_than: 0,
+      message: "1以上の数値を入力してください"
+    }
+
+  
+  validates :unit_type,
+    inclusion: { in: unit_types.keys }
+
+  validates :source,
+    inclusion: { in: sources.keys }
+
+  validates :jan_code,
+    format: {
+      with: /\A\d{13}\z/ ,
+      message: "13桁の数字を入力してください"
+    },
+    allow_blank: true
+    
+
+  validates :note,
+    length: { maximum: 500 },
+    allow_blank: true
+
+  private
+
+  def set_default_nutrition_values
+    self.kcal   ||= 0
+    self.protein ||= 0
+    self.fat    ||= 0
+    self.carbs  ||= 0
+    self.sugar  ||= 0
+    self.fiber  ||= 0
+  end
 end
